2017032c7fd6dba654c5edb313ea0b89570539ee
Michael Primavera <pastasauce59@gmail.com>
2021-03-27 15:01:00 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+main.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+
+//const victory = document.getElementById('victory');
+//victory.innerHTML = "<p>Michael is a champion</p>>";
+
+newHeader.innerHTML = "Michael is the champion";
